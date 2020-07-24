1a38106ed862297dd29b8db6f116ec8e33726f56
keerthi-reddy <r.keerthireddy74@gmail.com>
2020-07-25 00:49:39 +0530

    Removed comments in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,4 @@
-// import React from 'react'
-
-// const IndexPage = () => <h1>Hello world!</h1>
-
-// export default IndexPage
 import React from 'react'
-// import LandingComponent from '../components/LandingComponent'
 import TeamMemberComponent from '../components/TeamMemberComponent'
 const IndexPage = () => (
     <div>
