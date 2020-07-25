a37aceb2da64093325849780b7e1105662e36f1b
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2020-07-25 14:33:41 +0530

    refactor: moved styles from .css file to makeStyles

--- src/components/VisionMissionLayout.js
+++ src/components/VisionMissionLayout.js
@@ -1,36 +1,74 @@
-import React from "react"
-import {Typography, makeStyles } from "@material-ui/core"
+import React from 'react'
+import { Typography, makeStyles } from '@material-ui/core'
 import './VisionMissionStyles.css'
 
 const useStyles = makeStyles({
+  '@keyframes showTopText': {
+    '0%': { transform: 'translate(0, 100%)' },
+    '100%': { transform: 'translate(0, 0)' },
+  },
+
+  '@keyframes showBottomText': {
+    '0%': { transform: 'translate(0, -100%)' },
+    '100%': { transform: 'translate(0, 0)' },
+  },
+
   title: {
     fontFamily: "'Catamaran', sans-serif",
-    textTransform: "uppercase",
+    textTransform: 'uppercase',
+    bottom: 0,
+    animation: '$showTopText 1s 0.25s forwards',
+    transform: 'translate(0, 100%)',
   },
+
   body: {
-    fontFamily: "'Roboto', sans-serif", 
-    fontSize: 14,
+    fontFamily: "'Roboto', sans-serif",
+    fontSize: 15,
+    top: 0,
+    animation: '$showBottomText 1s 0.25s forwards',
+    transform: 'translate(0, -100%)',
+    paddingTop: '2vmin',
   },
+
   border: {
-    display: "block",
-    borderBottom: "8px solid black",
-    }
-});
+    display: 'block',
+    borderBottom: '5px solid black',
+  },
 
-export default function VisionMissionLayout(props) {
+  titleDiv: {
+    top: 0,
+  },
 
-  const classes = useStyles();
+  bodyDiv: {
+    bottom: 0,
+  },
+
+  layout: {
+    '& div': {
+      height: '50%',
+      overflow: 'hidden',
+      position: 'relative',
+      width: '100%',
+    },
+  },
+})
+
+export default function VisionMissionLayout({ title, body }) {
+  const classes = useStyles()
 
   return (
-    <div className="layout">
-        <div className="title">
-            <div><Typography variant="h2" className={classes.title}>{props.title}</Typography></div>
-        </div>
-        <div className={classes.border}></div>
-        <div className="body">
-            <div><Typography variant="caption" className={classes.body}>{props.body}</Typography></div>
-        </div>
+    <div className={classes.layout}>
+      <div className={classes.titleDiv}>
+        <Typography variant="h2" className={classes.title}>
+          {title}
+        </Typography>
+      </div>
+      <div className={classes.border} />
+      <div className={classes.bodyDiv}>
+        <Typography variant="body1" className={classes.body}>
+          {body}
+        </Typography>
+      </div>
     </div>
-  );
-  
+  )
 }
