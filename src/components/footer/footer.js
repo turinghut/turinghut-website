c8f2de0955545fc827c6112e868e2f1fd9ee0275
Sindhuja Ponnuru <sindhuja.ponnuru@gmail.com>
2020-07-28 19:04:05 +0530

    imported styles from another file, json changes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
+import footerStyles from './footer-styles'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import {
@@ -15,69 +15,56 @@ import {
 
 import FooterItem from './footer-item'
 
-const useStyles = makeStyles(theme => ({
-  footer: {
-    padding: theme.spacing(1, 1),
-    marginTop: 'auto',
-  },
-  paper: {
-    textAlign: 'center',
-  },
-}))
-
-const mapIcons = [
-  {
-    title: 'Maps',
-    icon: <Map />,
-    route: 'www.google.com',
-  },
-]
-const contactIcons = [
-  {
-    title: 'Email',
-    icon: <Email />,
-    route: 'www.google.com',
-  },
-  {
-    title: 'Phone',
-    icon: <LocalPhone />,
-    route: 'www.google.com',
-  },
-]
-const socialIcons = [
-  {
-    title: 'Github',
-    icon: <GitHub />,
-    route: 'https://github.com/turinghut',
-  },
-  {
-    title: 'facebook',
-    icon: <Facebook />,
-    route: '#', //replace with facebook link
-  },
-  {
-    title: 'instagram',
-    icon: <Instagram />,
-    route: 'https://www.instagram.com/turing.hut/',
-  },
-]
 const footerItems = [
   {
     title: 'Find us on Maps',
-    icons: mapIcons,
+    icons: [
+      {
+        title: 'Maps',
+        icon: <Map />,
+        route: 'www.google.com',
+      },
+    ],
   },
   {
     title: 'Contact us',
-    icons: contactIcons,
+    icons: [
+      {
+        title: 'Email',
+        icon: <Email />,
+        route: 'www.google.com',
+      },
+      {
+        title: 'Phone',
+        icon: <LocalPhone />,
+        route: 'www.google.com',
+      },
+    ],
   },
   {
     title: 'Follow us',
-    icons: socialIcons,
+    icons: [
+      {
+        title: 'Github',
+        icon: <GitHub />,
+        route: 'https://github.com/turinghut',
+      },
+      {
+        title: 'facebook',
+        icon: <Facebook />,
+        route: '#', //replace with facebook link
+      },
+      {
+        title: 'instagram',
+        icon: <Instagram />,
+        route: 'https://www.instagram.com/turing.hut/',
+      },
+    ],
   },
 ]
 
 const Footer = () => {
-  const styles = useStyles()
+  const styles = footerStyles()
   return (
     <footer className={styles.footer}>
       <Paper className={styles.paper}>
