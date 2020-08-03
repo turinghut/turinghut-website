cb473fab105305e5b3070ae60f0f3076818e3e43
Tarang Garlapally <taranggarlapally333@users.noreply.github.com>
2020-08-04 00:01:36 +0530

    added imageaddress to eventsdata

--- src/pages/components/Events.js
+++ src/pages/components/Events.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { EventsList } from "./EventsComps";
 import { Paper, Slide } from '@material-ui/core';
-import {upcomingEvent} from "./EventsData";
+import { upcomingEvent } from "./EventsData";
 import "./EventStyles.css";
 import Typography from '@material-ui/core/Typography';
 
@@ -15,13 +15,13 @@ export const Events = () => {
         <div className="upcomingEventTab">
             <h2 className="upcomingEventTitle">UPCOMING EVENT</h2>
             <Paper elevation={6} className="upcomingEventPaper">
-                <img className="upcomingEventImage" onClick={() => { setSlideIn(!slideIn) }} src="https://scontent.fymy1-1.fna.fbcdn.net/v/t1.0-9/s720x720/95913397_2869811633133317_2388660278012149760_o.jpg?_nc_cat=102&_nc_sid=2d5d41&_nc_eui2=AeE57UuBajJ7IzxJelCrb2vZh6YYytGVFjiHphjK0ZUWOOIOz73a2yHNddE8sqtkZDk&_nc_oc=AQlfv8OBWNuJ8hvASPxNjp-dfGr85BngsKDxe488kdVlnaJ5CUQxwjE7Lgi7ZFvqP--ek6z2ufC1cYgo7XVrF0Fx&_nc_ht=scontent.fymy1-1.fna&_nc_tp=7&oh=84cf1db715d8c8217c5921e9ed056a6b&oe=5F303002" />
+                <img className="upcomingEventImage" onClick={() => { setSlideIn(!slideIn) }} src={upcomingEvent.imageAddress} />
                 <Slide direction={"up"} in={slideIn} ><div className="details" onClick={() => { setSlideIn(!slideIn) }} className="upcomingEventTextOuter">
                     <div align="center" className="upcomingEventTextInner">
                         <Typography variant="h4">{upcomingEvent.name}</Typography>
                         <Typography variant="h5">{upcomingEvent.date}</Typography>
                         <br />
-                        <Typography variant="p">{upcomingEvent.startTime+" - "+upcomingEvent.endTime}</Typography>
+                        <Typography variant="p">{upcomingEvent.startTime + " - " + upcomingEvent.endTime}</Typography>
                     </div>
                 </div>
                 </Slide>
--- src/pages/components/EventsComps.jsx
+++ src/pages/components/EventsComps.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { Grid, Paper, Slide } from '@material-ui/core'
 import "./EventStyles.css";
-import {allEvents} from "./EventsData";
+import { allEvents } from "./EventsData";
 import Typography from '@material-ui/core/Typography';
 
 const EventTab = (props) => {
@@ -9,12 +9,12 @@ const EventTab = (props) => {
 
     return <Grid conatiner item xs={3} className="eventTab" >
         <Paper elevation={6} className="eventPaper">
-            <img className="eventImage" onClick={() => { setSlideIn(!slideIn) }} src="https://scontent.fymy1-1.fna.fbcdn.net/v/t1.0-9/s720x720/95913397_2869811633133317_2388660278012149760_o.jpg?_nc_cat=102&_nc_sid=2d5d41&_nc_eui2=AeE57UuBajJ7IzxJelCrb2vZh6YYytGVFjiHphjK0ZUWOOIOz73a2yHNddE8sqtkZDk&_nc_oc=AQlfv8OBWNuJ8hvASPxNjp-dfGr85BngsKDxe488kdVlnaJ5CUQxwjE7Lgi7ZFvqP--ek6z2ufC1cYgo7XVrF0Fx&_nc_ht=scontent.fymy1-1.fna&_nc_tp=7&oh=84cf1db715d8c8217c5921e9ed056a6b&oe=5F303002" />
+            <img className="eventImage" onClick={() => { setSlideIn(!slideIn) }} src={props.data.imageAddress} />
 
             <Slide direction={"down"} in={slideIn} ><div className="details" onClick={() => { setSlideIn(!slideIn) }} >
                 <div className="eventText">
-                <Typography variant="p">{props.data.name}</Typography><br />
-                <Typography variant="p">{props.data.date}</Typography>
+                    <Typography variant="p">{props.data.name}</Typography><br />
+                    <Typography variant="p">{props.data.date}</Typography>
                 </div>
             </div>
             </Slide>
@@ -24,5 +24,5 @@ const EventTab = (props) => {
 }
 
 export const EventsList = () => <Grid align="center" container item xs={12} spacing={4}>
-    {allEvents.map((data)=><EventTab data={data}/>)}
+    {allEvents.map((data) => <EventTab data={data} />)}
 </Grid>
