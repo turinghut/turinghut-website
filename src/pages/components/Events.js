6b02adae0ed7d3c016f90c2e87837de46fe7a7cf
Tarang Garlapally <taranggarlapally333@users.noreply.github.com>
2020-08-09 15:03:15 +0530

    changed most of the divs to grids

--- src/pages/components/Events.js
+++ src/pages/components/Events.js
@@ -1,33 +1,26 @@
-import React, { useState } from "react";
-import { EventsList } from "./EventsComps";
-import { Paper, Slide } from '@material-ui/core';
+import React from "react";
+import { EventsList, EventTab } from "./EventsComps";
 import { upcomingEvent } from "./EventsData";
 import "./EventStyles.css";
 import Typography from '@material-ui/core/Typography';
-
+import { Grid } from '@material-ui/core'
 
 export const Events = () => {
-    const [slideIn, setSlideIn] = useState(false);
 
-    return <div align="center" className="eventsComponent"><h2>EVENTS</h2>
 
-        <EventsList />
-        <div className="upcomingEventTab">
-            <h2 className="upcomingEventTitle">UPCOMING EVENT</h2>
-            <Paper elevation={6} className="upcomingEventPaper">
-                <img className="upcomingEventImage" onClick={() => { setSlideIn(!slideIn) }} src={upcomingEvent.imageAddress} />
-                <Slide direction={"up"} in={slideIn} ><div className="details" onClick={() => { setSlideIn(!slideIn) }} className="upcomingEventTextOuter">
-                    <div align="center" className="upcomingEventTextInner">
-                        <Typography variant="h4">{upcomingEvent.name}</Typography>
-                        <Typography variant="h5">{upcomingEvent.date}</Typography>
-                        <br />
-                        <Typography variant="p">{upcomingEvent.startTime + " - " + upcomingEvent.endTime}</Typography>
-                    </div>
-                </div>
-                </Slide>
+    // return <div align="center" className="eventsComponent">
+    return <Grid container style={{overflow:"hidden",marginTop:"20px"}}>
+        <Grid elevation={4} item xs={12}>
+            <Typography variant="h4" className="eventsTitle">EVENTS</Typography>
+            <EventsList />
+        </Grid>
+        <Grid item xs={12} style={{marginTop:"20px"}}>
+            <Typography style={{marginTop:"20px"}} variant="h4">UPCOMING EVENTS</Typography>
+        </Grid>
+        <Grid justify="center"  style={{marginBottom:"20px"}} container spacing={2}>
+            {upcomingEvent.map((data)=><Grid item xs={4} className="eventTab" key={data.key}><EventTab data={data} type="upcoming" /></Grid>)}
+       </Grid>
+    </Grid>
 
-            </Paper>
-        </div>
 
-    </div>
 }
--- src/pages/components/EventsComps.jsx
+++ src/pages/components/EventsComps.jsx
@@ -4,25 +4,42 @@ import "./EventStyles.css";
 import { allEvents } from "./EventsData";
 import Typography from '@material-ui/core/Typography';
 
-const EventTab = (props) => {
+export const EventTab = (props) => {
     const [slideIn, setSlideIn] = useState(false);
+    return <Paper elevation={6} className="eventPaper">
+        <img className={props.type === "past" ? "eventImage" : "upcomingEventImage"} onClick={() => { setSlideIn(!slideIn) }} src={props.data.imageAddress} />
 
-    return <Grid conatiner item xs={3} className="eventTab" >
-        <Paper elevation={6} className="eventPaper">
-            <img className="eventImage" onClick={() => { setSlideIn(!slideIn) }} src={props.data.imageAddress} />
+        <Slide direction={props.type === "past" ? "down" : "up"} in={slideIn} >
+            <div onClick={() => { setSlideIn(!slideIn) }} className={props.type === "past" ? "details" : "upcomingEventTextOuter"}>
+                {props.type === "past"
+                    ?
+                    <div className="eventText">
+                        <Typography variant="body2">{props.data.name}</Typography>
+                        <Typography variant="body2">{props.data.date}</Typography>
+                    </div>
+                    :
+                    <div className="upcomingEventTextInner">
+                        <Typography variant="h4">{props.data.name}</Typography>
+                        <Typography variant="h5">{props.data.date}</Typography>
+                        <br />
+                        <Typography variant="body2">{props.data.startTime + " - " + props.data.endTime}</Typography>
+                    </div>
+                }
 
-            <Slide direction={"down"} in={slideIn} ><div className="details" onClick={() => { setSlideIn(!slideIn) }} >
-                <div className="eventText">
-                    <Typography variant="p">{props.data.name}</Typography><br />
-                    <Typography variant="p">{props.data.date}</Typography>
-                </div>
             </div>
-            </Slide>
+        </Slide>
 
-        </Paper>
-    </Grid>
+    </Paper>
+
 }
 
-export const EventsList = () => <Grid align="center" container item xs={12} spacing={4}>
-    {allEvents.map((data) => <EventTab data={data} />)}
+
+
+
+export const EventsList = () => <Grid justify="center" container item xs={12} spacing={4}>
+    {allEvents.map((data) => (
+        <Grid item xs={3}  key={data.key}>
+            <EventTab data={data} type="past" />
+        </Grid>)
+    )}
 </Grid>
