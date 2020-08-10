0a94185cc66522876b2296da3c30b408e2190144
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2020-08-10 07:19:38 +0530

    created vision mission component (#15)
    * created vision mission component
    
    * changed animation delay
    
    * style: changed h1 to h2
    
    * refactor: moved styles from .css file to makeStyles
    
    * refactor: removed the empty grid element
    
    * removed carets in package.json
    
    * fix: animation on scroll
    
    * style: padding between vission&mission
    
    * refactor: kebab naming covention
    
    * useState instead of useRef for adding animation classes
    
    * fix: typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@material-ui/core'
 import Landing from '../components/landing'
+import VisionMissionComponent from '../components/vision-mission-component'
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 const IndexPage = () => (
-  <div>
+  <ThemeProvider theme={theme}>
     <Landing />
-  </div>
+    <VisionMissionComponent />
+  </ThemeProvider>
 )
 
 export default IndexPage
